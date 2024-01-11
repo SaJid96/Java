2a02c19da68f5eb73ee2829dfe10e780b82fd13d
SaJid961 <sajidhussain98@gmail.com>
2024-01-12 01:01:36 +0530

    test

--- JavaBasics/Patterns/test1.java
+++ JavaBasics/Patterns/test1.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
     int n=sc.nextInt();
     for (int i = 1; i <= n; i++) {
         for (int j = i; j <= n; j++) {
-            System.out.print("*");
+            System.out.print("**");
         }
         System.out.println();
     }
